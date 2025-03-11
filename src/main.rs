0f41cd97948582be65ebed5dfa0d2abe559a4704
Daniel Karlsson <daniel.e.karlsson@gmail.com>
2025-03-11 20:41:59 +0100

    add mindmap

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use clap::Parser;
+use clap::{Args, Parser, Subcommand};
 use serde_json::Value;
 use std::{
     fs::File,
     io::{BufReader, BufWriter, Write},
+    path::PathBuf,
 };
 
 #[derive(Parser, Debug)]
@@ -10,9 +11,12 @@ use std::{
 #[command(propagate_version = true)]
 struct Cli {
     /// Command
-    #[arg(value_enum)]
+    #[command(subcommand)]
     command: Commands,
+}
 
+#[derive(Args, Debug)]
+struct CommonArgs {
     /// Hide data elements
     #[arg(short, long)]
     elements_hide: bool,
@@ -25,48 +29,53 @@ struct Cli {
     files: Vec<String>,
 }
 
-#[derive(clap::ValueEnum, Clone, Debug)]
+#[derive(Subcommand, Debug)]
 enum Commands {
-    PlantUml,
-    // Mindmap {},
+    PlantUml(PlantUmlArgs),
+    Mindmap(MindmapArgs),
     // Table {}
 }
 
+#[derive(Args, Debug)]
+struct PlantUmlArgs {
+    #[command(flatten)]
+    common: CommonArgs,
+
+    #[arg(short, long, default_value = "output.plantuml")]
+    output_file: PathBuf,
+}
+
+#[derive(Args, Debug)]
+struct MindmapArgs {
+    #[command(flatten)]
+    common: CommonArgs,
+
+    #[arg(short, long, default_value_t = 255)]
+    box_level: usize,
+}
+
+#[derive(Debug)]
 struct ElementInfo {
     id: String,
     datatype: String,
     min: String,
-    max:String,
+    max: String,
 }
 
+#[derive(Debug)]
 struct DocInfo {
     id: String,
-    value: Value,
-//    elements: Vec<ElementInfo>,
+    // value: Value,
+    elements: Vec<ElementInfo>,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
 
-    println!("{:?}", cli);
-
-    let mut docs = Vec::<DocInfo>::new();
-
-    for file in cli.files.iter() {
-        if let Ok(value) = load_json_from_file(file) {
-            if let Some(id) = value["id"].as_str() {
-                let doc = DocInfo {
-                    id: id.to_string(),
-                    value: value.to_owned(),
-                };
-                docs.push(doc);
-            }
-        }
-    }
-
     match cli.command {
-        Commands::PlantUml {} => {
-            let output = File::create("output.plantuml")?;
+        Commands::PlantUml(args) => {
+            let docs = load_structure_definition_files(&args.common.files)?;
+            let output = File::create(args.output_file)?;
             let mut writer = BufWriter::new(output); // Create a buffered writer
 
             writeln!(
@@ -79,65 +88,39 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 writeln!(writer, "class **{}** {{", doc.id)?;
                 let mut relations = Vec::<(String, String, String, String)>::new();
 
-                if let Some(snapshot) = doc.value["snapshot"]["element"].as_array() {
-                    for element in snapshot.iter() {
-                        let element_id = element["id"].as_str().ok_or("Missing element id")?;
-                        if let Some(element_part) = get_slice_after_last_occurrence(element_id, '.')
-                        {
-                            let hier_level = count_char_occurrences(element_id, '.') * 2;
-                            // extract datatype doc.
-                            let mut datatype = element["type"][0]["code"]
-                                .as_str()
-                                .ok_or("Missing datatype")?;
-                            if datatype.starts_with("http") {
-                                datatype = get_slice_after_last_occurrence(datatype, '/')
-                                    .ok_or("Error in datatype")?;
-                            }
-
-                            // extract cardinality min and max values
-                            let min = if element["min"].is_string() {
-                                element["min"]
-                                    .as_str()
-                                    .ok_or(format!(
-                                        "Missing min cardinality: {:?}",
-                                        element["min"]
-                                    ))?
-                                    .to_string()
-                            } else {
-                                element["min"].to_string()
-                            };
-                            let max = if element["max"].is_string() {
-                                element["max"]
-                                    .as_str()
-                                    .ok_or(format!(
-                                        "Missing max cardinality: {:?}",
-                                        element["max"]
-                                    ))?
-                                    .to_string()
-                            } else {
-                                element["max"].to_string()
-                            };
-
-                            // if the datatype is one of the classes drawn, add a relation instead of a class element
-                            if let Some(_) = docs.iter().position(|s| s.id == datatype) {
-                                relations.push((
-                                    element_part.to_string(),
-                                    datatype.to_string(),
-                                    min.to_string(),
-                                    max.to_string(),
-                                ));
-                            } else {
-                                if !cli.elements_hide {
-                                    write!(
-                                        writer,
-                                        "{:>hier_level$}|_ {} : {}",
-                                        "", element_part, datatype
-                                    )?;
-                                    if !cli.cardinality_hide {
-                                        write!(writer, " [{}..{}]", min, max)?;
-                                    }
-                                    writeln!(writer)?;
+                for element in doc.elements.iter() {
+                    if let Some(element_part) = get_slice_after_last_occurrence(&element.id, '.') {
+                        let hier_level = count_char_occurrences(&element.id, '.') * 2;
+                        // extract datatype doc.
+                        let mut datatype = element.datatype.clone();
+                        if datatype.starts_with("http") {
+                            match get_slice_after_last_occurrence(&datatype, '/') {
+                                Some(dt) => {
+                                    datatype = dt;
                                 }
+                                None => {}
+                            };
+                        }
+
+                        // if the datatype is one of the classes drawn, add a relation instead of a class element
+                        if let Some(_) = docs.iter().position(|s| s.id == datatype) {
+                            relations.push((
+                                element_part,
+                                datatype,
+                                element.min.clone(),
+                                element.max.clone(),
+                            ));
+                        } else {
+                            if !args.common.elements_hide {
+                                write!(
+                                    writer,
+                                    "{:>hier_level$}|_ {} : {}",
+                                    "", element_part, datatype
+                                )?;
+                                if !args.common.cardinality_hide {
+                                    write!(writer, " [{}..{}]", element.min, element.max)?;
+                                }
+                                writeln!(writer)?;
                             }
                         }
                     }
@@ -158,6 +141,31 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
             writeln!(writer, "@enduml")?;
         }
+        Commands::Mindmap(args) => {
+            let docs = load_structure_definition_files(&args.common.files)?;
+            for doc in docs.iter() {
+                println!("processing: {}", doc.id);
+                let output = File::create(format!("{}.plantuml", doc.id))?;
+                let mut writer = BufWriter::new(output); // Create a buffered writer
+
+                writeln!(writer, "@startmindmap")?;
+
+                writeln!(writer, "* {}", doc.id)?;
+
+                for element in doc.elements.iter() {
+                    if let Some(element_part) = get_slice_after_last_occurrence(&element.id, '.') {
+                        let hier_level = count_char_occurrences(&element.id, '.') + 1;
+                        writeln!(writer, 
+                            "{}{} {}", 
+                            "*".repeat(hier_level), 
+                            if hier_level > args.box_level { "_" } else { "" },
+                            element_part)?;
+                    }
+                }
+
+                writeln!(writer, "@endmindmap")?;
+            }
+        }
     }
 
     Ok(())
@@ -170,14 +178,69 @@ fn load_json_from_file(path: &String) -> Result<Value, Box<dyn std::error::Error
     Ok(value)
 }
 
-fn get_slice_after_last_occurrence(s: &str, c: char) -> Option<&str> {
+fn get_slice_after_last_occurrence(s: &String, c: char) -> Option<String> {
     if let Some(last_index) = s.rfind(c) {
-        Some(&s[last_index + c.len_utf8()..])
+        Some(s[last_index + c.len_utf8()..].to_string())
     } else {
         None
     }
 }
 
-fn count_char_occurrences(s: &str, c: char) -> usize {
+fn count_char_occurrences(s: &String, c: char) -> usize {
     s.chars().filter(|&ch| ch == c).count()
+}
+
+fn load_structure_definition_files(
+    files: &Vec<String>,
+) -> Result<Vec<DocInfo>, Box<dyn std::error::Error>> {
+    let mut docs = Vec::<DocInfo>::new();
+    for file in files.iter() {
+        match load_single_structure_definition_file(file) {
+            Ok(doc_info) => {
+                docs.push(doc_info);
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
+    }
+    Ok(docs)
+}
+
+fn load_single_structure_definition_file(
+    file: &String,
+) -> Result<DocInfo, Box<dyn std::error::Error>> {
+    let doc = load_json_from_file(file)?;
+    let id = doc["id"].as_str().ok_or("Missing id")?;
+    let snapshot = doc["snapshot"]["element"]
+        .as_array()
+        .ok_or("Missing snapshot")?;
+    let mut elements = Vec::<ElementInfo>::new();
+    for element in snapshot.iter() {
+        let element_id = element["id"].as_str().ok_or("Missing element id")?;
+        if element_id != id {
+            let datatype = element["type"][0]["code"]
+                .as_str()
+                .ok_or("Missing datatype")?;
+            let min = if element["min"].is_string() {
+                element["min"]
+                    .as_str()
+                    .ok_or(format!("Missing min cardinality: {:?}", element["min"]))?
+                    .to_string()
+            } else {
+                element["min"].to_string()
+            };
+            let max = element["max"].as_str().ok_or("Missing max cardinality")?;
+            elements.push(ElementInfo {
+                id: element_id.to_string(),
+                datatype: datatype.to_string(),
+                min,
+                max: max.to_string(),
+            });
+        }
+    }
+    Ok(DocInfo {
+        id: id.to_string(),
+        elements,
+    })
 }
