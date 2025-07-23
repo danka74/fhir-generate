75838438b9350027a84ca683007788244d361871
Daniel Karlsson <daniel.e.karlsson@gmail.com>
2025-07-23 14:19:14 +0200

    add DR test data

--- tests/utils.rs
+++ tests/utils.rs
@@ -5,3 +5,16 @@ use std::fs;
 fn test_reduce_datatypes() {
     
 }
+
+#[test]
+fn test_next_alphabet_char() {
+    use next_alphabet_char;
+    assert_eq!(next_alphabet_char('A'), 'B');
+    assert_eq!(next_alphabet_char('Y'), 'Z');
+    assert_eq!(next_alphabet_char('Z'), 'A');
+    assert_eq!(next_alphabet_char('a'), 'b');
+    assert_eq!(next_alphabet_char('y'), 'z');
+    assert_eq!(next_alphabet_char('z'), 'a');
+    assert_eq!(next_alphabet_char('1'), 'A');
+    assert_eq!(next_alphabet_char('-'), 'A');
+}
